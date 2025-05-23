2cff2f1845e0d682a979d0fb022f5eb444bb96cf
rajakokate <rajakokate420@gmail.com>
2025-05-23 23:03:23 +0530

    feat: setup registration form with fields and select dropdowns

--- client/src/components/SelectField.jsx
+++ client/src/components/SelectField.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 const SelectField = ({ label, name, value, onChange, options }) => {
+  console.log(options)
   return (
     <div className="input-group">
       <label htmlFor={name}>{label}</label>
       <select id={name} name={name} value={value} onChange={onChange} required>
         <option value="">Select {label}</option>
         {options.map((opt) => (
-          <option key={opt} value={opt}>
-            {opt}
+          <option key={opt.value} value={opt}>
+            {opt.label}
           </option>
         ))}
       </select>
--- client/src/pages/Register/RegisterPage.jsx
+++ client/src/pages/Register/RegisterPage.jsx
@@ -1,101 +1,119 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Formik } from "formik";
 import InputField from "../../components/inputField";
 import SelectField from "../../components/SelectField";
 import styles from "./RegisterPage.module.css";
 
 const RegisterPage = () => {
   //initialize i18next app
-
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    phoneNumber: "",
-    department: "",
-    password: "",
-    confirmPassword: "",
-    role: "",
-  });
   const { t } = useTranslation();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-  };
+  // const [formData, setFormData] = useState();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setFormData((prev) => ({ ...prev, [name]: value }));
+  // };
+
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log(formData);
+  // };
   return (
     <div className={styles.container}>
       <h2>{t("register")}</h2>
-      <form onSubmit={handleSubmit}>
-        <InputField
-          label={t("form.username")}
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          placeholder={t("form.username")}
-        />
-        <InputField
-          label={t("form.email")}
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          placeholder={t("form.email")}
-        />
-        <InputField
-          label={t("form.phoneNumber")}
-          type="tel"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-          placeholder={t("form.phoneNumber")}
-        />
-        <SelectField
-          label={t("form.department")}
-          name="department"
-          value={formData.department}
-          onChange={handleChange}
-          options={[
-            t("departments.vfx"),
-            t("departments.paint"),
-            t("departments.roto")
-          ]}
-        />
-        <InputField
-          label={t("form.password")}
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          placeholder={t("form.password")}
-        />
-        <InputField
-          label={t("form.confirmPassword")}
-          type="password"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          placeholder={t("form.confirmPassword")}
-        />
-        <SelectField
-          label={t("form.role")}
-          name="role"
-          value={formData.role}
-          onChange={handleChange}
-          options={[
-            t("roles.projectManager"),
-            t("roles.supervisor"),
-            t("roles.teamlead"),
-            t("roles.artist"),
-          ]}
-        />
-        <button type="submit">{t("register")}</button>
-      </form>
+
+      <Formik
+        initialValues={{
+          username: "",
+          email: "",
+          phoneNumber: "",
+          department: "",
+          password: "",
+          confirmPassword: "",
+          role: "",
+        }}
+        onSubmit={(values) => {
+          console.log("Form Submitted", values);
+          //API Call logic here
+        }}
+      >
+        {({ values, handleChange, handleBlur, handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <InputField
+              label={t("form.username")}
+              type="text"
+              name="username"
+              value={values.username}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder={t("form.username")}
+            />
+            <InputField
+              label={t("form.email")}
+              type="email"
+              name="email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder={t("form.email")}
+            />
+            <InputField
+              label={t("form.phoneNumber")}
+              type="tel"
+              name="phoneNumber"
+              value={values.phoneNumber}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder={t("form.phoneNumber")}
+            />
+            <SelectField
+              label={t("form.department")}
+              name="department"
+              value={values.department}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              options={[
+                { value: "vfx", label: t("departments.vfx") },
+                { value: "paint", label: t("departments.paint") },
+                { value: "roto", label: t("departments.roto") },
+              ]}
+            />
+            <InputField
+              label={t("form.password")}
+              type="password"
+              name="password"
+              value={values.password}
+              onChange={handleChange}
+              placeholder={t("form.password")}
+            />
+            <InputField
+              label={t("form.confirmPassword")}
+              type="password"
+              name="confirmPassword"
+              value={values.confirmPassword}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              placeholder={t("form.confirmPassword")}
+            />
+            <SelectField
+              label={t("form.role")}
+              name="role"
+              value={values.role}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              options={[
+                { value: "projectManager", label: t("roles.projectManager") },
+                { value: "supervisor", label: t("roles.supervisor") },
+                { value: "teamlead", label: t("roles.teamlead") },
+                { value: "artist", label: t("roles.artist") },
+              ]}
+            />
+            <button type="submit">{t("register")}</button>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
